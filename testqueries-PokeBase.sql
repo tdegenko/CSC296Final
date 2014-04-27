2db391618176b1192572c167b9c9b1f5e045653e
cgonza13 <gonzalezyuki@gmail.com>
2014-04-27 16:44:30 -0400

    fixed second query
    First it had a syntax problem and also there wasn't a pokemon who met
    the query conditions.

--- testqueries-PokeBase.sql
+++ testqueries-PokeBase.sql
@@ -12,20 +12,33 @@ WHERE genCaught = 1 AND genIn = 2;
 /*
 *Query b: looking for a dark type pokemon that knows Surf
 */
-((SELECT pokedex FROM species
-WHERE type1 = "Dark" OR type2 = "Dark") JOIN
-pokemon ON pokedex) JOIN 
-(SELECT * FROM knows
-WHERE moveName1 = "Surf" OR moveName2 = "Surf" OR moveName3 = "Surf"
-OR moveName4 = "Surf");
+SELECT * 
+FROM (
+	SELECT pokemon . * 
+	FROM (
+		SELECT pokedex
+		FROM species
+		WHERE type1 =  "Dark"
+		OR type2 =  "Dark"
+	) AS spec
+	JOIN pokemon ON pokemon.pokedex = spec.pokedex
+) AS pkmn
+JOIN (
+	SELECT * 
+	FROM knows
+	WHERE moveName1 =  "Surf"
+	OR moveName2 =  "Surf"
+	OR moveName3 =  "Surf"
+	OR moveName4 =  "Surf"
+) AS mov;s
 
 /*
 *Query c: looking for a pokemon that can mate with Bidoof and
 *is in gen 3
 */
 SELECT * FROM pokemon JOIN 
-    (SELECT pokedex FROM species WHERE egg_group1 = e1 OR egg_group1 = e2
-    OR egg_group2 = e1 OR egg_group2 = e2 IN
+    (SELECT pokedex FROM species WHERE egg_group1 = "e1" OR egg_group1 = "e2"
+    OR egg_group2 = "e1" OR egg_group2 = "e2" IN
     (SELECT egg_group1 AS e1, egg_group2 AS e2 FROM species WHERE name = "Bidoof"))
 ON pokedex
 WHERE genIn = 3;
