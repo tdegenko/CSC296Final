c827b05a1b5de9abcd17482adba17e182aed7740
Tanner Degenkolb <t.degenk@gmail.com>
2014-04-28 20:02:09 -0400

    resolved merge conflicts

--- search_result.php
+++ search_result.php
@@ -1,32 +1,6 @@
 <?php
 //to do:use variables when DAL is complete
 require_once 'PokeDAL.php';
-
-<<<<<<< HEAD
-$ID=$_GET['ID'];
-$nickname=$_GET['nickname'];
-$gender=$_GET['gender'];
-$lvl=$_GET['level'];
-$happiness=$_GET['happiness'];
-$ability=$_GET['ability'];
-$nature=$_GET['nature'];
-$shiny=$_GET['shiny'];
-$HP=$_GET['HP'];
-$attack=$_GET['HP'];
-$defense=$_GET['HP'];
-$specialAttack=$_GET['HP'];
-$specialDefense=$_GET['HP'];
-$speed=$_GET['HP'];
-$originalTrainer=$_GET['originalTrainer'];
-$pokeball=$_GET['Pokeball'];
-$genIn=$_GET['genIn'];
-$genCaught=$_GET['genCaught'];
-$trainerName=$_GET['trainerName'];
-$pokedexNumber=$_GET['pokedex'];
-
-$tmp=pokemon::findByAttrs(array("nickname"=>$nickname,"gender"=$gender,"lvl"=$lvl,"happiness"=$happiness,"ability"=$ability,"nature"=$nature,"shiny"=$shiny,"HP"=$HP,"attack"=$attack,"defense"=$defense,"specialAttack"=$specialAttack,"specialDefense"=$specialDefense,"speed"=$speed,"originalTrainer"=$originalTrainer,"pokeball"=$pokeball,"genIn"=$genIn,"genCaught"=$genCaught,"trainerName"=$trainerName,"pokedexNumber"=$pokedexNumber))[0];
-print_r($tmp);
-=======
 $attrs=mapToAttrs($_GET);
 $pkmn=Pokemon::findByAttrs($attrs);
 ?>
@@ -189,4 +163,3 @@ function printPoke($pok){
     '</table>';
 }
 ?>
->>>>>>> 97c0db668a380e8b8cddc0e787d913c37288b53c
