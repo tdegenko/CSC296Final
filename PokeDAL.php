1b87832e0ce33da7403812cf21121307b8f117b6
Tanner Degenkolb <t.degenk@gmail.com>
2014-04-28 21:50:28 -0400

    interface and functionality for adding pokemon

--- PokeDAL.php
+++ PokeDAL.php
@@ -19,6 +19,8 @@ class Pokemon{
     //from pokemon
     private $ID, $originalTrainer, $nickname, $gender, $lvl, $trainerName, 
     $happiness, $ability, $nature, $shiny,  $HP, $attack, $defense, $specialAttack, $specialDefense, $speed, $accuracy, $evasion, $pokeball, $genIn, $genCaught, $itemName;
+    
+    static private $poke_attrs=["ID", "originalTrainer", "nickname", "gender", "lvl", "trainerName", "happiness", "ability", "nature", "shiny", "HP", "attack", "defense", "specialAttack", "specialDefense", "speed", "pokeball", "genIn", "genCaught", "pokedex", "itemName"];
 
     //from knows
     private $moveName1, $moveName2, $moveName3, $moveName4;
@@ -65,6 +67,68 @@ class Pokemon{
     static public function getRAttrs(){
         return self::$rattrs;
     }
+    //constructs a new pokemon and trys to write it to the DB. if it succeds returns the pokemon otherwise returns false;
+    function __construct($attrs){
+        try{
+            global $db;
+            foreach ($attrs as $key=>$value){
+                if(in_array($key,array_keys(get_object_vars($this))) and (!is_null($value)) and $value !=""){
+                    $this->$key=$value;
+                }
+            }
+            $db->beginTransaction();
+            $sql="INSERT INTO pokemon(
+                    ID, nickname, gender, lvl, happiness, ability, nature, shiny, HP,
+                    attack, defense, specialAttack,specialDefense, speed, originalTrainer, 
+                    pokeball, genIn, genCaught, trainerName, pokedex, itemName) ".
+            "VALUES (
+                    :ID, :nickname, :gender, :lvl, :happiness, :ability, :nature, :shiny, :HP,
+                    :attack, :defense, :specialAttack, :specialDefense, :speed, :originalTrainer, 
+                    :pokeball, :genIn, :genCaught, :trainerName, :pokedex, :itemName
+            ); ";
+            $params=array();
+            foreach(self::$poke_attrs as $key){
+                $params[$key]=$this->$key;
+            }
+            $stmt = $db->prepare($sql);
+            if(!$stmt){
+                $db->rollBack();
+                $error = "Could not add pokemon";
+                 throw new Exception($error);
+            }
+            if(!$stmt->execute($params)){
+                $db->rollBack();
+                $error = "Could not add pokemon";
+                 throw new Exception($error);
+            }
+            $sql="INSERT INTO knows 
+            (ID, originalTrainer, moveName1, moveName2,moveName3,moveName4) ".
+            "VALUES (:ID, :originalTrainer, :moveName1, :moveName2, :moveName3, :moveName4);";
+            $params=array(
+                ":ID"=>$this->ID,
+                ":originalTrainer"=>$this->originalTrainer
+            );
+            foreach(self::$move_rattrs as $key){
+                $params[$key]=$this->$key;
+            }
+            $stmt = $db->prepare($sql);
+            if(!$stmt){
+                $db->rollBack();
+                $error = "Could not add known moves";
+                throw new Exception($error);
+            }
+            if(!$stmt->execute($params)){
+                $db->rollBack();
+                $error = "Could not add known moves";
+                throw new Exception($error);
+            }
+            $db->commit();
+        }catch(PDOException $ex) {
+            echo("Could not find requested pokemon.\n");
+        }
+        
+
+    }
     //a more generic approach: pass an array of attributes
     //and add conditions to the query based on these attributes
     static public function findByAttrs($attrs){
@@ -78,7 +142,7 @@ class Pokemon{
             $any=false;
             $params=array();
             foreach ($attrs as $key=>$value){
-                if(in_array($key,self::$rattrs) and !(in_array($key,self::$move_rattrs))){
+                if(in_array($key,self::$rattrs) and !(in_array($key,self::$move_rattrs)) and (!is_null($value)) and $value !=""){
                     if($any){
                         $where.=" AND ";
                     }
@@ -119,11 +183,11 @@ class Pokemon{
             if ($any) {
                 $sql.=$where;
             }
-            $sql.=") AS sel JOIN knows ON sel.ID=knows.pokemonID AND sel.originalTrainer=knows.originalTrainer ";
+            $sql.=") AS sel JOIN knows ON sel.ID=knows.ID AND sel.originalTrainer=knows.originalTrainer ";
             $where="WHERE ";
             $any=false;
             foreach ($attrs as $key=>$value){
-                if(in_array($key,self::$rattrs) and (in_array($key,self::$move_rattrs)) and (!is_null($value)) and $value !="" ){
+                if(in_array($key,self::$rattrs) and (in_array($key,self::$move_rattrs)) and (!is_null($value)) and $value !=""){
                     echo $key."=>".$value."\n";
                     if($any){
                         $where.=" AND ";
