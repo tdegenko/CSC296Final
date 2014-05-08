4249bddbf34cf493f34c7bbc2532fd53c03e644f
Gerjo <gtacaray@u.rochester.edu>
2014-05-08 18:23:14 -0400

    prototype main requests page using new helper methods

---
+++ yourRequests.php
@@ -0,0 +1,57 @@
+<?php
+//to do:use variables when DAL is complete
+require_once 'include.php';
+$user=$_POST["trainerName"];
+$myReq=Requests::findByAttrs($attrs);
+?>
+
+<html>
+<head>
+<title>Your Requests!</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<a href="pokeSearch.php">Search for another Pokemon?</a>
+<a href="pokeEdit.php">Edit a Pokemon?</a>
+<a href="pokeAdd.php">Add a Pokemon?</a>
+<style>
+table{
+border-collapse:collapse;
+}
+table, th, td{
+border: 1px solid black;
+}
+</style>
+</head>
+
+<body>
+<h2>Your Requests</h2>
+
+<?php
+foreach($pkmn as $poke){
+    findMyRequests($user);
+}
+?>
+
+<h3>Requested of you</h3>
+<?php
+foreach($pkmn as $poke){
+    findRequested($user);
+}
+?>
+
+</body>
+</html>
+<?php
+function mapToAttrs($in){
+    $atts=array();
+    foreach ($in as $key=>$value){
+        if(!is_null($value) and $value != ""){
+            if(in_array($key,Pokemon::getRAttrs())){
+                $atts[$key]=$value;
+            }
+        }
+    }
+    return $atts;
+}
+
+?>
