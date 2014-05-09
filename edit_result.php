1eec0e7fc019e472729d50a37c7b8ab488ca4305
Gerjo <gtacaray@u.rochester.edu>
2014-05-09 00:12:25 -0400

    more links

--- edit_result.php
+++ edit_result.php
@@ -8,9 +8,10 @@ $pkmn=Pokemon::findByAttrs(array("ID"=>$_GET["ID"],"originalTrainer"=>$_GET["ori
     <head>
     <title>PokeTrader</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<a href="betaweb.csug.rochester.edu/~cdiaz3/Poke_Base/pokeEdit.php">Edit another Pokemon?</a> 
-	<a href="betaweb.csug.rochester.edu/~cdiaz3/Poke_Base/pokeAdd.php">Add a Pokemon?</a> 
-	<a href="betaweb.csug.rochester.edu/~cdiaz3/Poke_Base/pokeSearch.php">Search for a Pokemon?</a> 
+	<a href="pokeEdit.php">Edit another Pokemon?</a> 
+	<a href="pokeAdd.php">Add a Pokemon?</a> 
+	<a href="pokeSearch.php">Search for a Pokemon?</a> 
+	<a href="yourRequests.php">Look at your requests?</a> 
     <style>
         table{
             border-collapse:collapse;
